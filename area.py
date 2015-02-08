febf2e5a0f9a76790cb3f61733f126b0b39d54ed
Jon <jbeaulieu@mit.edu>
2015-02-08 16:32:26 -0500

    initial commit

---
+++ area.py
@@ -0,0 +1,13 @@
+#################################
+# Title: Area of a rectangle    #
+# Filename: area.py             #
+# Original Author: Jon Beaulieu #
+# Most Recent Edit: 2/3/2015    #
+# Last Editor: Jon Beaulieu     #
+#################################
+
+width = 20
+height = 30
+
+area = width * height
+print area
