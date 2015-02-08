370fad47f6e3bf8c997c41c2cea208ca7bdb8781
jbeaulieu <jbeaulieu@mit.edu>
2015-02-08 17:34:26 -0500

    adding vpython challenge 1

---
+++ challenge1.py
@@ -0,0 +1,21 @@
+#####################################
+# Title: VPython Video #1 Challenge #
+# Filename: challenge1.py           #
+# Original Author: Jon Beaulieu     #
+# Most Recent Edit: 2/8/2015        #
+# Last Editor: Jon Beaulieu         #
+#####################################
+
+from visual import *
+
+# Left sphere/arrow
+sphere(pos = vector(-1,0,0), radius = 0.5, color = color.green)
+arrow(pos = vector(-1,0,0), axis=vector(0,-1,0), color = color.red)
+
+# Left sphere/arrow
+sphere(pos = vector(1,1,0), radius = 0.5, color = color.green)
+arrow(pos = vector(1,1,0), axis=vector(-1,0,0), color = color.red)
+
+# Left sphere/arrow
+sphere(pos = vector(1,-1,0), radius = 0.5, color = color.green)
+arrow(pos = vector(1,-1,0), axis=vector(1,1,0), color = color.red)
